8ccbe2b229a21c4e2339422dbd40674d9d5b0026
YangShengchao <519395228@qq.com>
2020-04-10 13:30:33 +0800

    Fix log issue

--- Clutch/ClutchPrint.h
+++ Clutch/ClutchPrint.h
@@ -13,40 +13,23 @@ typedef NS_ENUM(NSUInteger, KJPrintLogLevel) {
     KJPrintLogLevelDebug = 2,
 };
 
-#define __NSLog(s, ...) do { \
-NSMutableString *logString = [NSMutableString stringWithFormat:@"[%@(%d)] ",[[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__]; \
-if (s) { \
-[logString appendFormat:(s), ##__VA_ARGS__]; \
-} \
-else { \
-    [logString appendString:@"(null)"]; \
-} \
-NSLog(@"%@", logString); \
+#define __Print(l, s, ...) do { \
+NSMutableString *logString = [NSMutableString stringWithFormat:@"[%@][%@(%d)] ",l, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__]; \
+if (s) { [logString appendFormat:(s), ##__VA_ARGS__]; } else { [logString appendString:@"(null)"]; } \
+printf("%s\n", logString.UTF8String); \
 } while(0);
 
-#define SGLog(...) __NSLog(__VA_ARGS__)
-
-
-
-
-#define __Print(l,f,n,c) do { \
-NSString *log = [NSString stringWithFormat:@"[%@][%@:%d]-> %@",l, [NSString stringWithUTF8String:f].lastPathComponent, n, c]; \
-printf("%s\n", log.UTF8String); \
-} while(0);
 
 #define KJPrint(...) do { \
-NSString *s = @"TODO: "; \
-__Print(@"Normal ", __FILE__, __LINE__, s) \
+__Print(@"Normal ", __VA_ARGS__) \
 } while(0);
 
 #define KJPrintVerbose(...) do { \
 if (KJPrintCurrentLogLevel < KJPrintLogLevelVerbose) { break; } \
-NSString *s = @"TODO: "; \
-__Print(@"Verbose", __FILE__, __LINE__, s) \
+__Print(@"Verbose", __VA_ARGS__) \
 } while(0);
 
 #define KJDebug(...) do { \
 if (KJPrintCurrentLogLevel < KJPrintLogLevelDebug) { break; } \
-NSString *s = @"TODO: "; \
-__Print(@"Debug  ", __FILE__, __LINE__, s) \
+__Print(@"Debug  ", __VA_ARGS__) \
 } while(0);
